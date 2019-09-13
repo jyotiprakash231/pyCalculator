3212dc3534c6a273919616fe886265da9d08575b
Jyoti Prakash <jpsahoo.edu@gmail.com>
2019-09-13 20:16:52 +0530

    Created a new python file

---
+++ pyCalculator.py
@@ -0,0 +1,14 @@
+print("**********pyCalculator**********")
+choice=int(input("\n1.Sum\n2.Sub\n3.Mul\n4.Div\nEnter your choice:"));
+firstNumber=int(input("\nEnter first number:"))
+secondNumber=int(input("\nEnter second number:"))
+if choice==1:
+    sum(firstNumber,secondNumber)
+elif choice==2:
+    sub(firstNumber,secondNumber)
+elif choice==3:
+    mul(firstNumber,secondNumber)
+elif choice==4:
+    div(firstNumber,secondNumber)
+else:
+    print("Please input valid choice")
